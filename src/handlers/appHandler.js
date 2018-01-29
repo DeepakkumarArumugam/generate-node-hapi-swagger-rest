7a054c906c158bbc4274af69acff1f4dc36e6a93
DeepakkumarArumugam <deepak.find2790@gmail.com>
2018-01-29 11:41:37 -0500

    Update appHandler.js

--- src/handlers/appHandler.js
+++ src/handlers/appHandler.js
@@ -1,37 +1,30 @@
 'use strict';
 
 const log = require('../config/logger'),
-    Feed = require('../models/feed');
+    Feed = require('../models/feed'),
+    ipfsAPI = require('ipfs-api');
+
+    // or using options
+const ipfs = ipfsAPI({host: 'localhost', port: '5001', protocol: 'http'})
 
 //exports
 module.exports = {
 
-    getFeeds: function (request, reply) {
-        const resultFeed = {};
-        resultFeed.data = [];
-        for (var i = 0; i < 5; i++) {
-            const feed = new Feed;
-            feed.feedId = "ABC" + Math.round(Math.random() * 1000);
-            feed.feedDesc = "this is feed description for " + feed.feedId;
-            resultFeed.data.push(feed);
-        }
-        log.debug({
-            resultFeed: resultFeed
-        }, 'feeds result');
-        reply(resultFeed);
+    getIpfsApiConfig: function (request, reply) {
+        ipfs.config.get((err, config) => {
+            if (err) {
+              throw err
+            }
+            reply(JSON.parse(config));
+          })
     },
-    getFeed: function (request, reply) {
-        const feedId = request.params.feedId;
-        const feed = new Feed;
-        feed.feedId = feedId;
-        feed.feedDesc = "this is feed description";
-        reply(feed);
+    getIpfsFilesList: function (request, reply) {
+        ipfs.ls.get('/ipfs/QmS4ustL54uo8FzR9455qaxZwuMiUhyvMcX9Ba8nUH4uVv',(err, response) => {
+            if (err) {
+              throw err
+            }
+            reply(JSON.parse(response));
+          })
     },
-    createFeed: function (request, reply) {
-            const reqData = request.payload;
-            const feed = new Feed;
-            feed.feedId = reqData.feedId;
-            feed.feedDesc = reqData.feedDesc;
-            reply(feed);
-        } //end of handler methods
+
 }; //end of module exports
